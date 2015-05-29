4d9086662daf793d5d727dafe955bb88ea54ea5f
Yazhong Liu <yorkiefixer@gmail.com>
2015-05-30 00:18:50 +0800

    add other modules

---
+++ src/builtin/datagram.rs
@@ -0,0 +1,6 @@
+
+use util::v8;
+
+pub fn Init() -> v8::Object {
+  v8::Object::New()
+}
--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -1,6 +1,18 @@
 
+pub mod buffer;
+pub mod console;
+pub mod crypto;
+pub mod datagram;
+pub mod dns;
+pub mod fs;
 pub mod http;
+pub mod https;
 pub mod module;
 pub mod net;
+pub mod os;
 pub mod path;
 pub mod process;
+pub mod querystring;
+pub mod readline;
+pub mod repl;
+pub mod url;

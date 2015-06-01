ee1e00c038bfeb216418249c7183d67844fd6567
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-02 07:25:32 +0800

    init with fs

---
+++ examples/fs.js
@@ -0,0 +1,5 @@
+
+var fs = require('fs');
+
+var r = fs.rename('./foobar', './foobar2');
+println(r);
--- src/builtin/fs.rs
+++ src/builtin/fs.rs
@@ -1,6 +1,54 @@
 
+use std::fs;
 use util::v8;
 
+extern fn rename(arguments: v8::FunctionCallbackInfo) {
+  let oldpath = arguments.At(0).ToString().as_string();
+  let newpath = arguments.At(1).ToString().as_string();
+  let retval = arguments.GetReturnValue();
+  match fs::rename(oldpath, newpath) {
+    Ok(()) => retval.SetWithBool(true),
+    Err(e) => retval.SetWithBool(false)
+  }
+}
+
+extern fn truncate(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn chown(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn rmdir(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn mkdir(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn readdir(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn readFile(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
+extern fn writeFile(arguments: v8::FunctionCallbackInfo) {
+  // TODO
+}
+
 pub fn Init() -> v8::Object {
-  v8::Object::New()
+  let exports = v8::Object::New();
+  exports.Set(v8::String::NewFromUtf8("rename"), v8::FunctionTemplate::New(rename).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("truncate"), v8::FunctionTemplate::New(truncate).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("chown"), v8::FunctionTemplate::New(chown).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("rmdir"), v8::FunctionTemplate::New(rmdir).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("mkdir"), v8::FunctionTemplate::New(mkdir).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("readdir"), v8::FunctionTemplate::New(readdir).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("readFile"), v8::FunctionTemplate::New(readFile).GetFunction());
+  exports.Set(v8::String::NewFromUtf8("writeFile"), v8::FunctionTemplate::New(writeFile).GetFunction());
+  return exports;
 }

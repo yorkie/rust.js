e3838e62a2c79462800e0646bc1f531c3a9cb1ff
Yazhong Liu <yorkiefixer@gmail.com>
2015-07-11 02:40:33 +0800

    init stringbytes

--- src/builtin/buffer.rs
+++ src/builtin/buffer.rs
@@ -1,5 +1,38 @@
+extern crate rustc_serialize;
 
 use util::v8;
+use self::rustc_serialize::hex::{FromHex, ToHex};
+use self::rustc_serialize::base64::{FromBase64, ToBase64, STANDARD};
+
+enum Encoding {
+  ASCII,
+  Binary,
+  UTF8,
+  UCS2,
+  Base64,
+  Hex
+}
+
+struct StringBytes {
+  source: String
+}
+
+impl StringBytes {
+  pub fn New(src: String) -> StringBytes {
+    StringBytes {
+      source: src
+    }
+  }
+  pub fn decode(&self, encoding: Encoding) -> Vec<u8> {
+    match encoding {
+      BASE64 => self.source.as_bytes().from_base64().unwrap()
+      // HEX => self.source.as_bytes().from_hex().unwrap(),
+    }
+  }
+  pub fn encode(&self, encoding: Encoding) {
+    // TODO
+  }
+}
 
 pub fn Init() -> v8::Object {
   v8::Object::New()
